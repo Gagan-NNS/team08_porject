1c2ce2c40e82ee8b133578fe3c95e1dfc920106a
muthu-raj-47 <muthunns890@gmail.com>
2025-04-07 20:22:09 +0530

    grade A was entre in code

---
+++ test/test001/markenter.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int marks[5];  // Array to store marks of 5 students
+    int i;
+
+    // Input marks
+    for(i = 0; i < 5; i++) {
+        printf("Enter marks for student %d: ", i + 1);
+        scanf("%d", &marks[i]);
+    }
+
+    // Evaluate marks
+    for(i = 0; i < 5; i++) {
+        printf("Student %d: ", i + 1);
+        
+        if(marks[i] >= 90) {
+            printf("Grade A\n");
+        } 
+    }
+
+    return 0;
+}
+
